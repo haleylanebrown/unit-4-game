3087edaed29b067f2428d17b2b03212c5f989230
haleylanebrown <47645417+haleylanebrown@users.noreply.github.com>
2019-03-05 20:09:12 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# unit-4-game
+Crystal Collector Game
+
+This game is my Unit 4 homework submission for Denver University's Coding Bootcamp.
+
+The objective of this assignment was to build an interactive game where the HTML is dynamically updated using jQuery.
+
+How to Play:
+- A random target score is generated on the page.
+- Four gems are also displayed as buttons, each with a different hidden value.
+- Each time the player clicks on a button, that hidden value is added to their score.
+- If the player matches their score to the target score, they win the game!
+- When the game is restarted, a new target score and gem values are assigned.
+
+Design Notes:
+- Gem buttons are assigned a number between 1 - 12
+- Random number generated is a number between 19 - 120
+
+Built With:
+- HTML5
+- CSS3
+- Twitter Bootstrap
+- Javascript
+- jQuery Library
---
+++ assets/javascript/script.js
@@ -0,0 +1,62 @@
+// VARIABLES
+var wins = 0;
+var losses = 0;
+var target = 0;
+var yourScore = 0;
+var crystals = [{image: "assets/images/crystal1.png", score: 0}, {image: "assets/images/crystal2.png", score: 0}, {image: "assets/images/crystal3.png", score: 0}, {image: "assets/images/crystal4.png", score: 0}];
+
+
+//FUNCTIONS
+function mathRandom(min, max) {
+    return Math.floor(Math.random () * max)+ min;
+}
+
+function crystalGeneration() {
+for (var i=0; i<crystals.length; i++) {
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", crystals[i].image);
+   
+    crystals[i].score = mathRandom(1, 12);
+    imageCrystal.attr("data-crystalvalue", crystals[i].score);
+    $("#crystalsPage").append(imageCrystal);
+}
+target = mathRandom(19, 120);
+$("#target-Score").text(target);
+}
+
+// reset all variables/clear
+function resetGame() {
+    target = 0;
+    yourScore = 0;
+    crystal = [];
+    $("#target-Score, #crystalsPage, #your-Score").text("");
+}
+
+
+//MAIN PROCESS
+crystalGeneration();
+
+$("#crystalsPage").on("click", ".crystal-image", function() {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    yourScore += parseInt(crystalValue);
+    $("#your-Score").text(yourScore)
+
+if (yourScore === target) {
+    alert("YOU WON!!");
+    wins++
+    $("#winsScore").text(wins);
+    resetGame();
+    crystalGeneration();
+}
+else if (yourScore > target) {
+    alert("You lost. Bummer.");
+    losses++
+    $("#lossesScore").text(losses);
+    resetGame();
+    crystalGeneration();
+}
+// jquery event delegation: allows me to hook an event to current elements and future elements
+
+
+})
